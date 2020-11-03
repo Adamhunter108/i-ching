54dc7a7f2f935338ceea51503a14de262b50ba87
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2020-11-02 22:45:36 -0500

    randomize feature works now

--- ichingmain.py
+++ ichingmain.py
@@ -1,7 +1,7 @@
 from ichingmath import Construct_Hexagram
 from hexagram_key import Trigrams, Hex_Meaning
 from hex_interpretation import hexagram_interpretations
-import random
+from random import choice
 
 
 # user input:
@@ -20,47 +20,53 @@ import random
 
 # coder input:
 
-myHexagram = Construct_Hexagram()
-myHexagram.addToss("h", "t", "t")
-myHexagram.addToss("h", "h", "h")
-myHexagram.addToss("t", "h", "t") 
-myHexagram.addToss("t", "t", "t") 
-myHexagram.addToss("h", "t", "h") 
-myHexagram.addToss("t", "t", "t") 
-myHexagram.loopThroughTosses()
-print(myHexagram.getHexagram())
-print(myHexagram.getTrigram())
+# myHexagram = Construct_Hexagram()
+# myHexagram.addToss("h", "t", "t")
+# myHexagram.addToss("h", "h", "h")
+# myHexagram.addToss("t", "h", "t") 
+# myHexagram.addToss("t", "t", "t") 
+# myHexagram.addToss("h", "t", "h") 
+# myHexagram.addToss("t", "t", "t") 
+# myHexagram.loopThroughTosses()
+# print(myHexagram.getHexagram())
+# print(myHexagram.getTrigram())
 
 
 # Hexagram interpretations:
 
-if "1" in myHexagram.getTrigram():
-	print(hexagram_interpretations["1"])
+# if "1" in myHexagram.getTrigram():
+# 	print(hexagram_interpretations["1"])
+
+# if "30" in myHexagram.getTrigram():
+# 	print(hexagram_interpretations["30"])
 
 
-# # randomized input
+# randomized input
 
-# randomTrigram = random.choice(list(Trigrams.items()))
-# randomHexagram  = randomTrigram + randomTrigram
-# print("Your automatic computer generated divinatory Hexagram is:\n" + str(randomHexagram)
+randomTrigram = choice(list(Trigrams.items()))
+randomHexagram  = randomTrigram + randomTrigram
+# print("Your automatic computer generated divinatory Hexagram is:\n" + str(randomHexagram))
 
-# trigram_values = list(Trigrams.values())
-# trigram_keys = list(Trigrams.keys())
+trigram_values = list(Trigrams.values())
+trigram_keys = list(Trigrams.keys())
 
-# upper_trigram = choice(trigram_values)
-# lower_trigram = choice(trigram_values)
+def getRandoTrigram(trigram_value):
+	trigram = choice(trigram_value)
+	line = trigram_keys[trigram_values.index(trigram)]
+	for li in line:
+		print(li)
+	return trigram
+upper = getRandoTrigram(trigram_values)
+lower = getRandoTrigram(trigram_values)
 
-# upper_lines = trigram_keys[trigram_values.index(upper_trigram)]
-# lower_lines = trigram_keys[trigram_values.index(lower_trigram)]
+hex_value = Hex_Meaning[upper, lower]
 
-# hex_value = Hex_Meaning[upper_trigram, lower_trigram]
+print("Upper: " + upper)
+print("Lower: " + lower)
 
-# for u_line in upper_lines:
-# 	print(u_line)
-# for l_line in lower_lines:
-# 	print(l_line)
+print("Hexagram Value: " + hex_value)
 
-# print("Upper: " + upper_trigram)
-# print("Lower: " + lower_trigram)
 
-# print("Hexagram Value: " + hex_value)
+
+
+
