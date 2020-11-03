ba83635635bdb7fa8a2852379a2f5c2b9d03ecb3
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2020-11-03 18:18:02 -0500

    added some interpretations and thinking about how to call them

--- ichingmain.py
+++ ichingmain.py
@@ -1,9 +1,10 @@
 from ichingmath import Construct_Hexagram
-from hexagram_key import Trigrams
-from hexagram_key import Hex_Meaning
+from hexagram_key import Trigrams, Hex_Meaning
+from hex_interpretation import hexagram_interpretations
+from random import choice
 
 
-# user input
+# user input:
 
 # myHexagram = Construct_Hexagram()
 # myHexagram.addToss(input("> "), input("> "), input("> "))
@@ -14,18 +15,56 @@ from hexagram_key import Hex_Meaning
 # myHexagram.addToss(input("> "), input("> "), input("> "))
 # myHexagram.loopThroughTosses()
 # print(myHexagram.getHexagram())
+# print(myHexagram.getTrigram())
 
-# coder input
 
-myHexagram = Construct_Hexagram()
-myHexagram.addToss("h", "t", "t")
-myHexagram.addToss("h", "h", "h")
-myHexagram.addToss("t", "h", "t") 
-myHexagram.addToss("t", "t", "t") 
-myHexagram.addToss("h", "t", "h") 
-myHexagram.addToss("t", "t", "t") 
-myHexagram.loopThroughTosses()
+# coder input:
 
-print(myHexagram.getHexagram())
+# myHexagram = Construct_Hexagram()
+# myHexagram.addToss("h", "t", "t")
+# myHexagram.addToss("h", "h", "h")
+# myHexagram.addToss("t", "h", "t") 
+# myHexagram.addToss("t", "t", "t") 
+# myHexagram.addToss("h", "t", "h")
+# myHexagram.addToss("t", "t", "t") 
+# myHexagram.loopThroughTosses()
+# print(myHexagram.getHexagram())
+# print(myHexagram.getTrigram())
 
-print(myHexagram.getTrigram())
+
+# randomized input
+
+randomTrigram = choice(list(Trigrams.items()))
+randomHexagram  = randomTrigram + randomTrigram
+trigram_values = list(Trigrams.values())
+trigram_keys = list(Trigrams.keys())
+def getRandoTrigram(trigram_value):
+	trigram = choice(trigram_value)
+	line = trigram_keys[trigram_values.index(trigram)]
+	for li in line:
+		print(li)
+	return trigram
+upper = getRandoTrigram(trigram_values)
+lower = getRandoTrigram(trigram_values)
+hex_value = Hex_Meaning[upper, lower]
+print("Upper: " + upper)
+print("Lower: " + lower)
+print("Hexagram Value: " + hex_value)
+
+# def Random_Hexagram(upper, lower):
+# 	pass
+
+# myHexagram = Random_Hexagram()
+
+# Hexagram interpretations:
+
+# if "1" in myHexagram.getTrigram():
+# 	print(hexagram_interpretations["1"])
+# if "2" in myHexagram.getTrigram():
+# 	print(hexagram_interpretations["2"])
+# if "3" and not "0" in myHexagram.getTrigram():
+# 	print(hexagram_interpretations["3"])
+# if "4" in myHexagram.getTrigram():
+# 	print(hexagram_interpretations["4"])
+
+
