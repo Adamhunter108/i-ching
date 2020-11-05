6e6d0e3dd35bd506c7e07eaa24b01d50609dd1be
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2020-11-05 14:59:36 -0500

    Update ichingmain.py

--- ichingmain.py
+++ ichingmain.py
@@ -83,27 +83,22 @@ if "1" in myHexagram.getTrigram():
 
 # randomized input
 
-# randomTrigram = choice(list(Trigrams.items()))
-# randomHexagram  = randomTrigram + randomTrigram
-# trigram_values = list(Trigrams.values())
-# trigram_keys = list(Trigrams.keys())
-# def getRandoTrigram(trigram_value):
-# 	trigram = choice(trigram_value)
-# 	line = trigram_keys[trigram_values.index(trigram)]
-# 	for li in line:
-# 		print(li)
-# 	return trigram
-# upper = getRandoTrigram(trigram_values)
-# lower = getRandoTrigram(trigram_values)
-# hex_value = Hex_Meaning[upper, lower]
-# print("Upper: " + upper)
-# print("Lower: " + lower)
-# print("Hexagram Value: " + hex_value)
+randomTrigram = choice(list(Trigrams.items()))
+randomHexagram  = randomTrigram + randomTrigram
+trigram_values = list(Trigrams.values())
+trigram_keys = list(Trigrams.keys())
+def getRandoTrigram(trigram_value):
+	trigram = choice(trigram_value)
+	line = trigram_keys[trigram_values.index(trigram)]
+	for li in line:
+		print(li)
+	return trigram
+upper = getRandoTrigram(trigram_values)
+lower = getRandoTrigram(trigram_values)
+hex_value = Hex_Meaning[upper, lower]
+myHexagram = (f"Upper: {upper}\nLower: {lower}\nHexagram Value: {hex_value}")
+print(myHexagram)
 
-# def Random_Hexagram(upper, lower):
-# 	pass
-
-# myHexagram = Random_Hexagram()
 
 # Hexagram interpretations:
 
